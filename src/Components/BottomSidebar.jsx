9601ba70d4de1e117c8ff397b9f7fcecb27329b9
abolfazl <abol.costom.1@gmail.com>
2022-07-18 02:18:53 +0430

    new update - update z-index - Abol

--- src/Components/BottomSidebar.jsx
+++ src/Components/BottomSidebar.jsx
@@ -22,7 +22,7 @@ const BottomSidebar = () => {
 
   return (
     <>
-      <div className="fixed bottom-0 flex h-16 w-full items-center justify-around border-t-4 border-[#212121] bg-[#0E0E0E] md:hidden">
+      <div className="fixed bottom-0 z-50 flex h-16 w-full items-center justify-around border-t-4 border-[#212121] bg-[#0E0E0E] md:hidden">
         {sideBarLinks.map((link) => (
           <Link to={link.path} className={"text-center"}>
             <link.icon
